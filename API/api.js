d1d49fdb79dfb226287d3e4630c1adc5d845803a
alishajain <jain.alisha14@gmail.com>
2022-04-10 16:40:31 +0530

    Added styling to the page

--- API/api.js
+++ API/api.js
@@ -3,14 +3,12 @@ var output = document.querySelector("#output");
 var btn = document.querySelector("#submit");
 var baseUrl = "https://api.funtranslations.com/translate/yoda.json";
 
-//fetch(baseUrl).then((response) => console.log(response.json));
-
  function clickHandler() {
      var url = baseUrl + "/?text=" + input.value;
 
      fetch(url)
          .then((response) => response.json())
-         .then((data) => console.log(data));
+        .then((data) => (output.innerHTML = data.contents.translated));
  }
 
 btn.addEventListener("click", clickHandler);
