9db485e168df794becc2d0626363856ee5487459
alishajain <jain.alisha14@gmail.com>
2022-04-14 13:42:43 +0530

    palindrome logic build

--- question8/q8.js
+++ question8/q8.js
@@ -2,36 +2,77 @@
 var bday = document.querySelector("#dob");
 var btn = document.querySelector("#submit");
 var output = document.querySelector("#output");
+var count = 0;
 
-function revDate(date){
-    revBday = date.split('').reverse().join('');
-    console.log(revBday);
-}
-
-function getResult(){
-    var date_sum = bday.value.split("-");
-    var dob;
-
-    dob = date_sum[0] + date_sum[1] + date_sum[2];
-    console.log(dob);
-
-    revBday = dob.split('').reverse().join('');
-    console.log(revBday);
-
-    var i = 0, count = 0;
+function isPalindrome(dob) {
+    var i = 0;
     while(i <= 3) {
-        if(dob[i] === revBday[i]){
+        if(dob[i] === dob[7-i]){
+            count = 0;
             i++;
         } else {
             count = 1;
             break;
         }
     }
+    return count;
+}
 
-    if(count === 0 ){
+function closestSmallPalindrome(dob) {
+    var sPNum = dob;
+    
+    while(count === 1) {
+        sPNum++;
+        isPalindrome(sPNum.toString());
+    }
+    console.log(sPNum);
+    count = 1;
+    return sPNum;
+}
+
+function closestLargePalindrome(dob) {
+    var lPNum = dob;
+    while(count === 1) {
+        lPNum--;
+        isPalindrome(lPNum.toString());
+    }
+    console.log(lPNum);
+    return lPNum;
+}
+
+function closestPalindrome(dob){
+    var sP = 0, lP = 0, sPDiff, lPdiff, num;
+    num = dob;
+    sP = closestSmallPalindrome(num);
+    lP = closestLargePalindrome(num);
+
+    sPDiff = Math.abs(sP - dob);
+    lPdiff = Math.abs(lP - dob);
+    console.log(sPDiff);
+    console.log(lPdiff);
+
+    if(sPDiff > lPdiff){
+        return lP;
+    } else {
+        return sP;
+    }
+}
+
+function getResult(){
+    var date_sum = bday.value.split("-");
+    var dob, result, newPNum;
+
+    dob = date_sum[0] + date_sum[1] + date_sum[2];
+    console.log(dob);
+    
+    result = isPalindrome(dob);
+    dob = Number(dob);
+    if(result === 0 ){
         output.innerHTML = "Your birthday is a palindrome!!!";
     } else {
         output.innerHTML = "Your birthday is not a palindrome!!!";
+        newPNum = closestPalindrome(dob.toString());
+        console.log(newPNum);
     }
 }
 
